55656de8b517d2e195c2a674ad87bee11ba39b83
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-05-26 18:57:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const makeChange = (c) => {
   // your name here
+	let den = ["q","d","n","p"]
+let cur =[25,10,5,1]
+let obj = {}
+
+for(let i=0;i<den.length;i++){
+    let count = 0
+    while(c>=cur[i]){
+        c -= cur[i]
+        count+=1
+    }
+    obj[den[i]]=count
+}
+return obj
 };
 
 // Do not the change the code below
